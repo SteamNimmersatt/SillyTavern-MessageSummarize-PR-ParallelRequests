9d2b6352bf05c21de5e7a70b0c53377614583161
qvink <qvink@users.noreply.github.com>
2025-10-07 23:13:12 -0600

    following move of getRegexScripts, and updating ST version dependency.

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ import { dragElement } from '../../../RossAscends-mods.js';
 import { debounce_timeout } from '../../../constants.js';
 import { MacrosParser } from '../../../macros.js';
 import { commonEnumProviders } from '../../../slash-commands/SlashCommandCommonEnumsProvider.js';
-import { getRegexScripts } from '../../../../scripts/extensions/regex/index.js'
+import { getRegexScripts } from '../../../../scripts/extensions/regex/engine.js'
 import { runRegexScript } from '../../../../scripts/extensions/regex/engine.js'
 import { t, translate } from '../../../i18n.js';
 
